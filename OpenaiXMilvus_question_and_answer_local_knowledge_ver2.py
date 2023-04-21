f1a9f758c14d6a81c4afdc2a28134d77b0097dfd
Tawan Tantikul <70018928@THAIBEV.COM>
2023-04-21 15:27:54 +0700

    remove credentials

--- OpenaiXMilvus_question_and_answer_local_knowledge_ver2.py
+++ OpenaiXMilvus_question_and_answer_local_knowledge_ver2.py
@@ -173,10 +173,10 @@ def Vectorize_DataFrame(dfIn, columnList, start_id):
 ### connect to pymilvus
 #### set up Milvus according to the documentation on its website , standalone : https://milvus.io/docs/install_standalone-operator.md
 #### input host ip , ip of machine with Milvus installed
-connections.connect("default", host="10.8.222.129", port="19530")   
+connections.connect("default", host="xx.xx.xx.xx", port="19530")   
 
 ## openai
-openai.api_key='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'    #### input your oopenai API key here
+openai.api_key='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'    #### input your oopenai API key here
 
 EMBEDDING_MODEL = "text-embedding-ada-002"
 COMPLETIONS_MODEL = "gpt-3.5-turbo"
